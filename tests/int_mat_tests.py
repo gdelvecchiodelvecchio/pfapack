0f1e7e0da16abc65c4af59fea51d72c756d69933
Giuseppe Del Vecchio Del Vecchio <giuseppedelvecchiodelvecchio@gmail.com>
2021-09-16 17:42:06 +0200

    Fixed tests

--- tests/int_mat_tests.py
+++ tests/int_mat_tests.py
@@ -7,12 +7,12 @@ def int_rand_mat(n):
     return A - A.T
 
 def float_rand_mat(n):
-    A = np.random.rand(n,n)*10
+    A = np.random.rand(n,n)
     return A - A.T
 
 
 def complex_rand_mat(n):
-    A = np.random.rand(n,n)*10 + np.random.rand(n,n)*10j
+    A = np.random.rand(n,n) + np.random.rand(n,n)*1j
     return A - A.T
 
 
@@ -20,30 +20,43 @@ def int_rand_mat2(n):
     A=np.matrix(list(map(np.around, np.random.rand(n,n)*10)))
     return A - A.T
 
-
+nmax = 200
+dn = 20
+nmin = 40
 print("Testing integer matrices...")
-for i in np.arange(2,12,2):
-    A = int_rand_mat(6)
+for n in np.arange(nmin, nmax, dn):
+    print("n=%i" % (n))
+    A = int_rand_mat(n)
     H = pf.pfaffian(A, method="H")
     P = pf.pfaffian(A, method="P")
-    print("Method H: ", H ** 2 - np.linalg.det(A))
-    print("Method P: ", P ** 2 - np.linalg.det(A))
+    da = np.linalg.det(A)
+    #pritn("pfaff(A)=%i" % (da))
+    print("Method H: ", np.log10(H ** 2) - np.log10(da))
+    print("Method P: ", np.log10(P ** 2) - np.log10(da))
+    print("\n")
 
 print("Testing real matrices...")
-for i in np.arange(2,12,2):
-    A = float_rand_mat(6)
+for n in np.arange(nmin, nmax, dn):
+    print("n=%i \n" % (n))
+    A = float_rand_mat(n)
     H = pf.pfaffian(A, method="H")
     P = pf.pfaffian(A, method="P")
-    print("Method H: ", H ** 2 - np.linalg.det(A))
-    print("Method P: ", P ** 2 - np.linalg.det(A))
-
+    da = np.linalg.det(A)
+    # pritn("pfaff(A)=%i" % (da))
+    print("Method H: ", np.log10(H ** 2) - np.log10(da))
+    print("Method P: ", np.log10(P ** 2) - np.log10(da))
+    print("\n")
 
 
 print("Testing complex matrices...")
-for i in np.arange(2,12,2):
-    A = complex_rand_mat(6)
+for n in np.arange(nmin, nmax, dn):
+    print("n=%i \n"%(n))
+    A = complex_rand_mat(n)
     H = pf.pfaffian(A, method="H")
     P = pf.pfaffian(A, method="P")
-    print("Method H: ", H ** 2 - np.linalg.det(A))
-    print("Method P: ", P ** 2 - np.linalg.det(A))
+    da = np.linalg.det(A)
+    # pritn("pfaff(A)=%i" % (da))
+    print("Method H: ", np.log10(H ** 2) - np.log10(da))
+    print("Method P: ", np.log10(P ** 2) - np.log10(da))
+    print("\n")
 
